46c08dc2c5ba6c30486a2166a4276b238db2b6b8
your-name <184022686+stupid-nerd-01111000@users.noreply.github.com>
2024-12-26 18:08:34 +0330

    fix the code

--- Two-Sum/solution.py
+++ Two-Sum/solution.py
@@ -2,16 +2,15 @@ class Solution():
     @staticmethod
     def twoSum(nums, target):
         for num in range(len(nums) - 1):
-            zz = nums[num] + nums[num + 1]
-            if zz == target:
-                return [num, num + 1]
+            for num2 in range(num + 1, len(nums)):
+                if nums[num] + nums[num2] == target:
+                        return [num, num2]
         return [-1, -1]
 
 
+numso = [3, 2, 3]
 
-numso = [1, 1, 3, 3, 5, 6, 7, 8, 4, 1]
-
-targeto = 5
+targeto = 6
 
 
 result = Solution.twoSum(nums=numso, target=targeto)
